cd82b62aba2aa376df239b369d1fd0b69c04f248
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-12 11:07:51 +0200

    models added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :movies do
+    resources :bookmarks
+  end
+  resources :lists do
+    resources :bookmarks
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
