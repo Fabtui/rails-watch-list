3cadf82d9d6b60ad163d0e375cb3f1b66f6662d9
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-09-15 11:26:36 +0200

    add user references to models

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  belongs_to :user
   has_one_attached :photo
 end
---
+++ db/migrate/20210915092426_add_user_ref_to_bookmarks.rb
@@ -0,0 +1,5 @@
+class AddUserRefToBookmarks < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :bookmarks, :user, null: false, foreign_key: true
+  end
+end
