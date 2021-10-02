c9d991864bccd567b38acaae684c97584df2c5f9
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-10-02 17:46:55 +0200

    new list style

---
+++ app/javascript/movie-react/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import FlatList from '../containers/flatlist'
+// import Map from '../containers/map'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="row">
+        <h1>Coucou</h1>
+        {/* <FlatList />
+        <Map /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ app/javascript/movie-react/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise';
+
+
+import '../assets/stylesheets/application.scss';
+import App from './components/app';
+
+// import flatsReducer from './reducers/flats_reducer';
+// import selectedFlatReducer from './reducers/selected_flat_reducer';
+
+const reducers = combineReducers({
+  // flats: flatsReducer,
+  // selectedFlat: selectedFlatReducer
+});
+
+const middlewares = applyMiddleware(logger, reduxPromise);
+
+ReactDOM.render(
+  console.log('ok')
+  <Provider store={createStore(reducers, {}, middlewares)}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
