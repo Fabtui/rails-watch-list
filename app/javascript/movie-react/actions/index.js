f94479532a8ba149582f2f48b134022395ac89de
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-10-04 12:13:49 +0200

    add react with api in movie index

--- app/javascript/movie-react/actions/index.js
+++ app/javascript/movie-react/actions/index.js
@@ -4,13 +4,17 @@ export const SET_MOVIES = 'SET_MOVIES';
 export const MOVIE_SELECTED = 'MOVIE_SELECTED';
 
 export function setMovies() {
+  const promise = fetch('/api/v1/movies')
+    .then(response => response.json())
   return {
     type: SET_MOVIES,
-    payload: movies
+    payload: promise
   };
 }
 
 export default function selectMovie(movie) {
+  const promise = fetch(`/api/v1/movies/${movie}`)
+    .then(response => response.json())
   return {
     type: MOVIE_SELECTED,
     payload: movie
